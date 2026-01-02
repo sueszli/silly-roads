04dcb589889f17abf50fda7fa7b749f09f8fc9ad
sueszli <jabaryyahya@gmail.com>
2026-01-03 00:19:39 +0100

    up

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@ int main() {
     hello_from_lib();
 
     InitWindow(800, 450, "raylib [core] example - basic window");
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Press ESC to exit gracefully", 190, 230, 20, DARKGRAY);
         EndDrawing();
     }
 
